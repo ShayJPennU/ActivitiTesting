d55bea2d7d95cbe6c3e709c1b60fd3963751123c
Jacob Shay <Jacob_Shay@pennunited.com>
2014-07-10 09:04:53 -0400

    first commit

---
+++ src/test/java/MyUnitTest.java
@@ -0,0 +1,36 @@
+
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+	
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+	
+	@Test
+	@Deployment(resources = {"org/activiti/test/my-process.bpmn20.xml"})
+	public void test() {
+		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
+		assertNotNull(processInstance);
+		
+		Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
+		
+		
+		
+	   String outcome = getVariableLocal("outcome");
+		
+		
+		
+		
+		
+		
+	}
+
+}
